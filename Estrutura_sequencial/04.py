6b51e97f966d224863aa2de37c2ffca1dbc2db8d
vitavares <vitoriadesousat@gmail.com>
2022-09-01 17:38:29 -0300

    Adicionei os primeiros 5 testes.

---
+++ Estrutura_sequencial/04.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+
+n1 = int(input('Primeira nota: '))
+n2 = int(input('Segunda nota: '))
+n3 = int(input('Terceira nota: '))
+n4 = int(input('Quarta nota: '))
+
+media = (n1+n2+n3+n4)/4
+
+print(f'Média: {media}')
---
+++ Estrutura_sequencial/05.py
@@ -0,0 +1,6 @@
+#Faça um Programa que converta metros para centímetros. 
+metro = float(input('Qual medida você deseja converter? '))
+
+cen = metro * 100
+
+print(f'{metro} metros equivalem a {cen} centímetros.')
