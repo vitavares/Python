22923cf270efe63ebc5989e10a4f93a4585bb0d1
Vitória Tavares <65176795+vitavares@users.noreply.github.com>
2022-09-07 16:50:44 -0300

    Update 05.py

--- Estrutura_sequencial/05.py
+++ Estrutura_sequencial/05.py
@@ -1,6 +1,5 @@
 #Faça um Programa que converta metros para centímetros. 
-metro = float(input('Qual medida você deseja converter? '))
 
+metro = float(input('Medida (Metros: ) '))
 cen = metro * 100
-
-print(f'{metro} metros equivalem a {cen} centímetros.')
+print(f'{metro} metros equivalem a {round(cen, 2)} centímetros.')
