589dc29c3ca914cc203b27b0c77fd177ce5cb22c
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-12 16:37:24 -0300

    Delete Estrutura-Sequencial directory

--- Estrutura-Sequencial/05.py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que converta metros para centímetros. 
-
-metro = float(input('Medida (Metros: ) '))
-cen = metro * 100
-print(f'{metro} metros equivalem a {round(cen, 2)} centímetros.')
--- Estrutura-Sequencial/07.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
-
-lado = float(input('Lado em metros: '))
-dobro = (lado ** 2) * 2
-print(f'O dobro da área deste quadrado é {round(dobro, 2)}')
