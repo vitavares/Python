58ac138033cb009cfcc74aa3e6c43c6968b59a14
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-12 16:37:36 -0300

    Delete Funções directory

--- Funções/teste2.py
+++
@@ -1,9 +0,0 @@
-def dados(nome, idade=None):
-    print('Nome: {}'.format(nome))
-    if idade is not None:
-        print('Idade: {}'.format(idade))
-    else:
-        print('Idade não informada.')
-
-dados('ana', 17)
-dados('josé')
