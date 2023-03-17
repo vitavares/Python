9411c4c5dc78a71e60695081daa89d02db648f84
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-17 17:00:34 -0300

    Resolução de exercício

---
+++ Exercícios/exe3.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor == segundo_valor:
+    print('\nOs valores são iguais.')
+elif primeiro_valor > segundo_valor:
+    print('\nO primeiro valor é maior.')
+else:
+    print('O segundo valor é maior.')
