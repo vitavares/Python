ba8b54185ae5b3debcc2caa28f97d278e9ae0f75
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-19 20:10:35 -0300

    Testes de formatação de strings

---
+++ Testes/fstring.py
@@ -0,0 +1,12 @@
+variavel = 'V'
+
+# Padding
+print(f'{variavel}') #normal
+print(f'{variavel: >11}') #esquerda
+print(f'{variavel: <11}') #direita
+print(f'{variavel: ^11}') #centro
+
+
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
---
+++ Testes/interpolacao.py
@@ -0,0 +1,4 @@
+nome = 'vitória'
+valor = 1000.5248
+exibir = '%s, o preço é RS$%.2f' % (nome, valor)
+print(exibir)
