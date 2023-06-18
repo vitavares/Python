a88981607f10d784936d9c4c640dc170348f3134
vitavares <vitoriadesousat@gmail.com>
2023-06-17 21:14:18 -0300

    teste

---
+++ Testes/else.py
@@ -0,0 +1,17 @@
+#
+# while
+#
+count = 0
+while count <= 5:
+    print(count)
+    count += 1
+else:
+    print('após iteração')
+
+#
+# for
+#
+for i in range(5):
+    print(i)
+else:
+    print('após iteração')
